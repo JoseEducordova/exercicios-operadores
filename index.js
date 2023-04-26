60ad3d60f277326b7bb61539bd9830215b61481b
JoseEducordova <josejojocarvalho@gmail.com>
2023-04-25 21:51:28 -0300

    exercicios

---
+++ index.js
@@ -0,0 +1,41 @@
+// + adição
+// - subtração
+// * multiplicação 
+// / divisão
+// % resto divisão
+
+/*const primeirovalor = 3
+const segundovalor = 4
+const terceirovalor = 5
+resultadoum = primeirovalor + segundovalor
+console.log(resultadoum)
+
+resultadodois = (primeirovalor * terceirovalor) / 2
+console.log(resultadodois)
+
+resultadotres = (segundovalor - terceirovalor) * -1
+console.log(resultadotres)
+
+resultadoquatro = 234 % terceirovalor
+console.log(resultadoquatro)*/
+
+/*console.log( 1 === 1 )
+console.log( 1 === 2 )*/
+
+/*const primeironumero = 10
+const segundonumero = 20
+console.log(primeironumero === segundonumero)
+console.log(primeironumero !== segundonumero)
+console.log(primeironumero > segundonumero)
+console.log(primeironumero >= segundonumero)
+console.log(primeironumero < segundonumero)
+console.log(primeironumero <= segundonumero)*/
+
+const nome = prompt("Qual seu nome?")
+const idade = Number (prompt("Qual sua idade?"))
+const idadeum = idade + 27
+
+console.log("Nome:", nome)
+console.log("Idade:", idade) 
+console.log ("Você é maior de idade:", idade > 18)
+console.log ("Você terá", idadeum ," de idade em 2050")
