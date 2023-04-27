4d1e83067ac46365fa125f805a715f057e9f074c
JoseEducordova <josejojocarvalho@gmail.com>
2023-04-26 21:22:28 -0300

    exercicios aula 26/04

--- index.js
+++ index.js
@@ -1,41 +1,67 @@
-// + adição
-// - subtração
-// * multiplicação 
-// / divisão
-// % resto divisão
+/*const bool1 = true
+const bool2 = false
+const bool3 = !bool2
 
-/*const primeirovalor = 3
-const segundovalor = 4
-const terceirovalor = 5
-resultadoum = primeirovalor + segundovalor
-console.log(resultadoum)
+let resultado = bool1 && bool2
+console.log("a. ", resultado)
 
-resultadodois = (primeirovalor * terceirovalor) / 2
-console.log(resultadodois)
+resultado = bool1 && bool2 && bool3 
+console.log("b. ", resultado) 
 
-resultadotres = (segundovalor - terceirovalor) * -1
-console.log(resultadotres)
+resultado = !resultado && (bool1 || bool2) 
+console.log("c. ", resultado)
 
-resultadoquatro = 234 % terceirovalor
-console.log(resultadoquatro)*/
+console.log("d. ", typeof resultado)*/
 
-/*console.log( 1 === 1 )
-console.log( 1 === 2 )*/
+/*let primeiroNumero = Number(prompt("Digite um numero!"))
+let segundoNumero = Number(prompt("Digite outro numero!"))
 
-/*const primeironumero = 10
-const segundonumero = 20
-console.log(primeironumero === segundonumero)
-console.log(primeironumero !== segundonumero)
-console.log(primeironumero > segundonumero)
-console.log(primeironumero >= segundonumero)
-console.log(primeironumero < segundonumero)
-console.log(primeironumero <= segundonumero)*/
+const soma = primeiroNumero + segundoNumero
 
-const nome = prompt("Qual seu nome?")
-const idade = Number (prompt("Qual sua idade?"))
-const idadeum = idade + 27
+console.log(soma)*/
 
-console.log("Nome:", nome)
-console.log("Idade:", idade) 
-console.log ("Você é maior de idade:", idade > 18)
-console.log ("Você terá", idadeum ," de idade em 2050")
+
+/*const idade = Number(prompt("Qual sua idade? "))
+const idadeum = Number(prompt("Qual a idade do/a seu/sua melhor amigo/a"))
+const resultado = idade > idadeum
+const resultadodois = idade - idadeum
+
+console.log("Sua idade é maior do que a do seu melhor amigo?", resultado)
+console.log("A diferença entre a idade de voces é de: ", resultadodois)*/
+
+
+/*let numeropar = Number(prompt("insira um numero par:"))
+
+resultado = numeropar % 2
+
+console.log("O numero par é: ", resultado)*/
+
+//ambas as tentativas resultavam em 0...como se independente do numero que 
+//fosse colocado resultaria em 0. Após tais tentativas com numero impar, o resultado passou a ser 1.
+
+
+/*const idade = Number(prompt("Digite sua idade:" ))
+
+resultadodias = idade * 365
+resultadomeses = idade * 12
+resultadohoras = idade * 8760
+
+console.log("Sua idade em dias é de:", resultadodias)
+console.log("Sua idade em meses é de:", resultadomeses)
+console.log("Sua idade em horas é de:", resultadohoras)*/
+
+let numeroum = Number(prompt("insira um numero:"))
+let numerodois = Number(prompt("insira um segundo numero:"))
+
+resultadoum = numeroum > numerodois
+resultadodois = numeroum === numerodois
+resultadotres = numeroum % numerodois == 0 
+resultadoquatro = numerodois % numeroum == 0 
+
+console.log("O primeiro numero é maior que segundo?", resultadoum)
+console.log("O primeiro numero é igual ao segundo?", resultadodois)
+console.log("O primeiro numero é divisível pelo segundo?", resultadotres)
+console.log("O segundo numero é divisível pelo primeiro?", resultadoquatro)
+
+
+
